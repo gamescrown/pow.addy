fdcb7645811ae3c99c0d5b765f13be2a23d0145d
onur <onur@pow.games>
2022-06-15 13:27:08 +0300

    feat: Integrate applovin consent flow for android

--- Runtime/AppLovinMaxManager.cs
+++ Runtime/AppLovinMaxManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using pow.aidkit;
 using pow.hermes;
 using UnityEngine;
@@ -14,6 +15,8 @@ namespace pow.addy
     public class AppLovinMaxManager : Singleton<AppLovinMaxManager>
     {
         [SerializeField] private AdEventHandler adEventHandler;
+        [SerializeField] private Policies policies;
+        [SerializeField] private GameEvent onSetUserConsentStatus;
         [SerializeField] internal string maxSdkKey;
 
         [Header("IOS")] [SerializeField] internal string bannerIdIOS;
@@ -50,7 +53,6 @@ namespace pow.addy
             MaxSdkCallbacks.OnSdkInitializedEvent += sdkConfiguration =>
             {
                 // AppLovin SDK is initialized, start loading ads
-
                 _interstitialController.InitializeInterstitialAds();
                 _rewardedController.InitializeRewardedAds();
                 _bannerController.InitializeBannerAds();
@@ -58,23 +60,42 @@ namespace pow.addy
                 MaxSdk.SetVerboseLogging(true);
 
                 // TODO: Remove on release build
-                //MaxSdk.ShowMediationDebugger();
+                MaxSdk.ShowMediationDebugger();
+            };
 
+            MaxSdkCallbacks.OnSdkInitializedEvent += (sdkConfiguration) =>
+            {
+                if (sdkConfiguration.ConsentDialogState == MaxSdkBase.ConsentDialogState.Applies)
+                {
+                    // Show user consent dialog
 
-//                if (sdkConfiguration.ConsentDialogState == MaxSdkBase.ConsentDialogState.Applies)
-//                {
-//#if UNITY_ANDROID
-//                    MaxSdk.UserService.ShowConsentDialog();
-//#endif
-//                }
-//                else if (sdkConfiguration.ConsentDialogState == MaxSdkBase.ConsentDialogState.DoesNotApply)
-//                {
-//                    // No need to show consent dialog, proceed with initialization
-//                }
+#if UNITY_ANDROID
+                    print("[ApplovinMAX] MaxSdkBase.ConsentDialogState.Applies ShowConsentDialog");
+                    MaxSdk.UserService.ShowConsentDialog();
+#endif
+                }
+                else if (sdkConfiguration.ConsentDialogState == MaxSdkBase.ConsentDialogState.DoesNotApply)
+                {
+                    // No need to show consent dialog, proceed with initialization
+                    print("[ApplovinMAX] MaxSdkBase.ConsentDialogState.DoesNotApply");
+                }
+                else
+                {
+                    // Consent dialog state is unknown. Proceed with initialization, but check if the consent
+                    // dialog should be shown on the next application initialization
+                    print("[ApplovinMAX] MaxSdkBase.ConsentDialogState.Unknown");
+                }
             };
 
             MaxSdk.SetSdkKey(maxSdkKey);
             MaxSdk.InitializeSdk();
+        }
+
+        private IEnumerator OnSetUserConsentStatus()
+        {
+            yield return new WaitUntil(() => MaxSdk.IsUserConsentSet());
+            print($"[ApplovinMAX] OnSetUserConsentStatus");
+            onSetUserConsentStatus?.Invoke();
         }
 
         public void ShowInterstitial(InterstitialTag interstitialTag, Action onCompleteAction)
