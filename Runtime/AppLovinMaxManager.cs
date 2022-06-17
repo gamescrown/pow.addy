4f5401fadb9464b3fabc3e634f4a80bddc93e791
onur <onur@pow.games>
2022-06-17 16:13:46 +0300

    feat: Enable debugger

--- Runtime/AppLovinMaxManager.cs
+++ Runtime/AppLovinMaxManager.cs
@@ -80,7 +80,7 @@ namespace pow.addy
                 MaxSdk.SetVerboseLogging(true);
 
                 // TODO: Remove on release build
-                //MaxSdk.ShowMediationDebugger();
+                MaxSdk.ShowMediationDebugger();
             };
 
             MaxSdkCallbacks.OnSdkInitializedEvent += (sdkConfiguration) =>
